41f0251f61f0f2c44a59d9b96ccdf6fc29e7f7f0
Can <can.gel@hotmail.com>
2023-02-25 20:08:23 +0300

    SignInpage created.

---
+++ src/pages/accountPages/signinPages/signinPages.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import style from './style'
+
+export const SigninPages = () => {
+    return (
+        <View style={style.container}>
+            <Text>signinPages</Text>
+        </View>
+    )
+}
