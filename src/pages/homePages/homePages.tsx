74e60faee3dd59b369fbd86f929eac9c201b47aa
Can <can.gel@hotmail.com>
2023-02-25 19:25:45 +0300

    Homepage created.

---
+++ src/pages/homePages/homePages.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import style from './style'
+
+export const HomePages = () => {
+    return (
+        <View style={style.container}>
+            <Text>homePages</Text>
+        </View>
+    )
+}
---
+++ src/pages/homePages/index.ts
@@ -0,0 +1,1 @@
+export { HomePages } from "./homePages"
