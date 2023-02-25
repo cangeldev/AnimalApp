95dfe388fd26d4f993f1809a8160ce2e54413fa7
Can <can.gel@hotmail.com>
2023-02-25 19:30:41 +0300

    Profilepage created.

---
+++ src/pages/profilePages/index.ts
@@ -0,0 +1,1 @@
+export { ProfilePages } from "./profilePages"
---
+++ src/pages/profilePages/profilePages.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import style from './style'
+
+export const ProfilePages = () => {
+    return (
+        <View style={style.container}>
+            <Text>profilePages</Text>
+        </View>
+    )
+}
