79296776509ed197bd0479a88a2aa9eb6d16723f
Can <can.gel@hotmail.com>
2023-03-01 12:49:28 +0300

    BottomTab icon added.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,10 @@ export const Twitter = require("./images/twitter.png");
 export const Facebook = require("./images/facebook.png");
 export const Google = require("./images/google.png");
 export const BackgroundPaw = require("./images/backgroundPaw.png");
+export const ProfileSelected = require("./images/profileSelected.png");
+export const Profile = require("./images/profile.png");
+export const HutSelected = require("./images/hutSelected.png");
+export const Hut = require("./images/hut.png");
 export const PetsLottie = require("./images/pets.json")
 export const PawsLottie = require("./images/paw.json")
 
--- src/navigation/tabNavigator.tsx
+++ src/navigation/tabNavigator.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Image } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomePages, ProfilePages } from '../pages';
+import { Hut, HutSelected, Profile, ProfileSelected } from '../assets';
 
 export const TabNavigator = () => {
     const Tab = createBottomTabNavigator();
@@ -8,14 +10,47 @@ export const TabNavigator = () => {
     return (
         <Tab.Navigator
             initialRouteName='HomePages'
-            screenOptions={{ headerShown: false }}>
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false
+            }}>
             <Tab.Screen
                 name="HomePages"
                 component={HomePages}
+                options={{
+                    tabBarIcon: ({ focused }) => focused ?
+                        <Image
+                            source={HutSelected}
+                            style={{
+                                width: 28,
+                                height: 28,
+                            }} /> :
+                        <Image
+                            source={Hut}
+                            style={{
+                                width: 28,
+                                height: 28,
+                            }} />,
+                }}
             />
             <Tab.Screen
                 name="ProfilePages"
                 component={ProfilePages}
+                options={{
+                    tabBarIcon: ({ focused }) => focused ?
+                        <Image
+                            source={ProfileSelected}
+                            style={{
+                                width: 30,
+                                height: 30,
+                            }} /> :
+                        <Image
+                            source={Profile}
+                            style={{
+                                width: 30,
+                                height: 30,
+                            }} />,
+                }}
             />
         </Tab.Navigator>
     )
